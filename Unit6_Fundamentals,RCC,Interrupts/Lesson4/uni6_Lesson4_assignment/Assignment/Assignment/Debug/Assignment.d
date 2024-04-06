0bf825a81a6aa12e13e0fbb24fdfa048cce6b3f1
Seifeldin-Ahmed <120275931+Seifeldin-Ahmed@users.noreply.github.com>
2024-04-06 23:42:56 +0200

    Assignment of unit6 lesson4

---
+++ Unit6_Fundamentals,RCC,Interrupts/Lesson4/uni6_Lesson4_assignment/Assignment/Assignment/Debug/Assignment.d
@@ -0,0 +1,53 @@
+Assignment.d Assignment.o: .././Assignment.c \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom32.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/interrupt.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay_basic.h \
+ e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/math.h \
+ .././BIT_utils.h .././STD_Types.h .././EXT_INT_int.h
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/io.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/sfr_defs.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom32.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/version.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/fuse.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/lock.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/interrupt.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/util/delay_basic.h:
+
+e:\courses\embedded\ system\ iti\iti\ training\ programms\atmel\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/math.h:
+
+.././BIT_utils.h:
+
+.././STD_Types.h:
+
+.././EXT_INT_int.h:
